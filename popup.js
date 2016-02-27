11c31364fba59388270ef373d910a508761c9b75
bchoukri <cfbadr@gmail.com>
2016-02-27 19:05:26 +0100

    merge

--- popup.js
+++ popup.js
@@ -1,13 +1,20 @@
 
 var ref = new Firebase('https://burning-torch-2085.firebaseio.com/');
 
-ref.authWithPassword({
-  email    : email,
-  password : password
-}, function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
+
+var submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", connect)
+
+function connect() {
+	alert("test")
+	ref.authWithPassword({
+  	email    : $("#email").val(),
+  	password : $("#password").val()
+	}, function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+    	console.log("Authenticated successfully with payload:", authData);
+  	}
+	});
+}
